94610a5deb3b08fbab3b21a9b211d191da19276d
Ana Malikovski <amalikovski@gmail.com>
2020-12-25 21:48:16 -0300

    Inclusão de alguns valores nas tabelas na inicialização

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into country (id_ibge_country, name_country) values (1058, 'Brasil')
+
+insert into state (id_ibge_state, id_ibge_country, name_state, uf_state) values (43, 1058, 'Rio Grande do Sul', 'RS')
+
+insert into city (id_ibge_city, id_ibge_state, name_city) values (4311403, 43, 'Lajeado')
